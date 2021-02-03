0ed8d067bed2c006f5923b4ec37afa6ff761665c
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-02-02 22:10:03 -0600

    Added functionality to form

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const printToDom = (divId, textToPrint) => {
 	selectedDiv.innerHTML = textToPrint;
 };
 
-const studentForm = () => {
+const showStudentForm = () => {
 	let form = '';
 	form += `
       <form>
@@ -19,16 +19,31 @@ const studentForm = () => {
           <label for="studentNames" class="col-sm-2 col-form-label">Students</label>
           <div class="col-sm-10">
             <input type="text" class="form-control" id="inputName" placeholder="Wizard Name" required>
-            <button type="submit" class="btn btn-primary" id="sort-input-btn">Sort Me!</button>
           </div>
         </div>
+        <button type="submit" class="btn btn-primary" id="sort-input-btn">Sort Me!</button>
       </form>`;
 
 	printToDom('#students-container', form);
+	document.querySelector('form').addEventListener('submit', getStudentForm);
+};
+
+const getStudentForm = (e) => {
+	e.preventDefault();
+	console.log('form submitted');
+	const name = document.querySelector('#inputName').value;
+	const house = hogwartsHouses[Math.floor(Math.random() * hogwartsHouses.length)];
+	const studentObj = {
+		name,
+		house
+	};
+	students.push(studentObj);
+	console.log(students);
+	document.querySelector('form').reset();
 };
 
 const buttonEvents = () => {
-	document.querySelector('#sorting-btn').addEventListener('click', studentForm);
+	document.querySelector('#sorting-btn').addEventListener('click', showStudentForm);
 };
 
 const init = () => {
